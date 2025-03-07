fcc86b4a897567ab0d882ee3a78ae0774b04ad18
Shiver1155 <hif7f6dyfdt@gmail.com>
2025-03-06 21:15:17 -0300

    test 1

---
+++ TIPOSVARIAVEIS/test.java
@@ -0,0 +1,13 @@
+package TIPOSVARIAVEIS;
+
+
+public class test {
+    public static void main (String [] args) {
+        int year = 23;
+        String name = "Wesley";
+
+        System.out.println("ano: " + year + " Nome: " + name);
+        
+
+    }
+}
--- TIPOSVARIAVEIS/tiposVariaveis.java
+++ TIPOSVARIAVEIS/tiposVariaveis.java
@@ -3,6 +3,17 @@ package TIPOSVARIAVEIS;
 class tiposVariaveis {
 
     public static void main(String [] args) {
-        System.out.println("Ola Mundo");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21077333;
+        long cpf = 98765432109L;
+        final float pi = 3.14f;
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(idade);
     }
 }
